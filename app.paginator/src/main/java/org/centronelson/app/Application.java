856c531214aa9b35d374c0edf8d31a6f4ce2fb5d
lmora <lmora.fp@gmail.com>
2019-02-06 15:18:18 +0100

    Second Commit

--- app.paginator/src/main/java/org/centronelson/app/Application.java
+++ app.paginator/src/main/java/org/centronelson/app/Application.java
@@ -2,7 +2,7 @@ package org.centronelson.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//<Second commit>
 @SpringBootApplication
 public class Application {
 
